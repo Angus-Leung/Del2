9a5c3954d34adb429a9fbfc12e2772bcc357cbbc
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-10-30 00:55:33 -0400

    resolved conflicts

--- Admin.cc
+++ Admin.cc
@@ -1,6 +1,5 @@
 #include "Admin.h"
 
-<<<<<<< HEAD
 Admin::Admin() {
   name = "Default Admin";
 }
@@ -12,39 +11,4 @@ Admin::Admin(string n) {
 string Admin::getName() {
   return name;
 }
-=======
-#include <iostream>
-using namespace std;
 
-public:
-	Admin();
-	Admin(string);
-	string getName();
-	Projet* getProject(string);
-
-private:
-	string name;
-	Queue<Project*> myProjects;
-
-Admin::Admin() {
-	name = "Unknown";
-}
-
-Admin::Admin(string n) {
-	name = n;
-}
-
-string Admin::getName() {
-	//cout << "\n StudentName: " << name << "\n";
-	return name;
-}
-
-Project* Admin::getProject(string n) {
-	for (int i = 0; myProjects[i] != NULL; i++) {
-		if (myProjects[i]->getName == n) {
-			return myProjects[i];
-		}
-	}
-	return 0;
-}
->>>>>>> origin/master
